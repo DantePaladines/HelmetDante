0418c491ceadf64a398ad470b3b4bd6d17900354
DantePc <djkmecdgm65@gmail.com>
2023-11-10 20:31:41 -0500

    agregando helmet para desabilitar x-powered-by

--- myApp.js
+++ myApp.js
@@ -1,60 +1,24 @@
 const express = require('express');
 const app = express();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const helmet = require("helmet")
 
 module.exports = app;
 const api = require('./server.js');
+
 app.use(express.static('public'));
+
+//Desabilitamos X-Powered-By, este header nos permite ver en que esta desarrollado le server de la aplicacion web.
+app.use(helmet.hidePoweredBy())
+
 app.disable('strict-transport-security');
+
 app.use('/_api', api);
+
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
+
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Your app is listening on port ${port}`);
--- server.js
+++ server.js
@@ -5,8 +5,6 @@
 
 var express = require("express");
 var app = express();
-const helmet = require("helmet")
-helmet.hidePoweredBy("x-powered-by")
 app.disable("x-powered-by");
 var fs = require("fs");
 var path = require("path");
